b85b7d0c481b6402e60bd196c149a1250aa2a864
Ian <kesler@unlv.nevada.edu>
2020-07-06 15:54:37 -0700

    Add script that loads mnist data and performs gradient descent

---
+++ src/learn.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 14:57:13 2020
+learn.py
+~~~~~~~~~~
+Loads mnist data and performs gradient descent.
+~~~~~~~~~~
+@author: Ian
+"""
+
+import mnist_loader
+import network
+
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = network.Network([784, 30, 10])
+epochs = 30
+batch_size = 10
+eta = 3.0
+net.SGD(training_data, epochs, batch_size, eta, test_data=test_data)
